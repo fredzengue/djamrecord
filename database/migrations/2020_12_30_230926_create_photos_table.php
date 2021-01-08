bf212a0bf41eee9997678e36f698764469011a81
unknown <fredzengue@gmail.com>
2021-01-08 18:55:25 +0100

    final2

--- database/migrations/2020_12_30_230926_create_photos_table.php
+++ database/migrations/2020_12_30_230926_create_photos_table.php
@@ -16,6 +16,7 @@ class CreatePhotosTable extends Migration
         Schema::create('photos', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->string('url');
             $table->string('poster');
             $table->timestamps();
         });
--- database/migrations/2020_12_30_231007_create_videos_table.php
+++ database/migrations/2020_12_30_231007_create_videos_table.php
@@ -16,6 +16,7 @@ class CreateVideosTable extends Migration
         Schema::create('videos', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->string('url');
             $table->date('release_date');
             $table->string('poster');
             $table->string('description');
