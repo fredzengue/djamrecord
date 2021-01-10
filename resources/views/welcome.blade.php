aaaed4270bf4f4df3c38319544e437d9cb748231
unknown <fredzengue@gmail.com>
2021-01-11 00:10:59 +0100

    test final

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,10 +8,10 @@
   <div class="row">
   <div class="col-lg-6">
   <div class="hs-text">
-  <h2><span>Music</span> for everyone.</h2>
+  <h2><span>Musique</span> pour tous.</h2>
   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. </p>
-  <a href="#" class="site-btn">Download Now</a>
-  <a href="#" class="site-btn sb-c2">Start free trial</a>
+  <a href="#" class="site-btn">Inscription</a>
+  <a href="#" class="site-btn sb-c2">onnexion</a>
   </div>
   </div>
   <div class="col-lg-6">
@@ -27,10 +27,10 @@
   <div class="row">
   <div class="col-lg-6">
   <div class="hs-text">
-  <h2><span>Listen </span> to new music.</h2>
+  <h2><span>Ecouter </span> la nouvelle musique.</h2>
   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. </p>
-  <a href="#" class="site-btn">Download Now</a>
-  <a href="#" class="site-btn sb-c2">Start free trial</a>
+  <a href="#" class="site-btn">Connexion</a>
+  <a href="#" class="site-btn sb-c2">Inscription</a>
   </div>
   </div>
   <div class="col-lg-6">
@@ -50,7 +50,7 @@
 <div class="row">
 <div class="col-lg-6">
 <div class="section-title">
-<h2>Unlimited Access to 100K tracks</h2>
+<h2>Un accès ilimité à nos pistes</h2>
 </div>
 </div>
 <div class="col-lg-6">
@@ -65,7 +65,7 @@
 <section class="how-section spad set-bg" data-setbg="https://preview.colorlib.com/theme/solmusic/img/how-to-bg.jpg">
 <div class="container text-white">
 <div class="section-title">
-<h2>How it works</h2>
+<h2>Comment ça marche</h2>
 </div>
 <div class="row">
 <div class="col-md-4">
@@ -73,7 +73,7 @@
 <div class="hi-icon">
 <img src="https://preview.colorlib.com/theme/solmusic/img/icons/brain.png" alt="">
 </div>
-<h4>Create an account</h4>
+<h4>Créer un compte</h4>
 <p>Lorem ipsum dolor sit amet, consectetur adipi-scing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum sus-pendisse ultrices gravida. </p>
 </div>
 </div>
@@ -82,7 +82,7 @@
 <div class="hi-icon">
 <img src="https://preview.colorlib.com/theme/solmusic/img/icons/pointer.png" alt="">
 </div>
-<h4>Choose a plan</h4>
+<h4>Choisir un abonnement</h4>
 <p>Donec in sodales dui, a blandit nunc. Pellen-tesque id eros venenatis, sollicitudin neque sodales, vehicula nibh. Nam massa odio, portti-tor vitae efficitur non. </p>
 </div>
 </div>
@@ -91,7 +91,7 @@
 <div class="hi-icon">
 <img src="https://preview.colorlib.com/theme/solmusic/img/icons/smartphone.png" alt="">
 </div>
-<h4>Download Music</h4>
+<h4>Télécharger</h4>
 <p>Ablandit nunc. Pellentesque id eros venenatis, sollicitudin neque sodales, vehicula nibh. Nam massa odio, porttitor vitae efficitur non, ultric-ies volutpat tellus. </p>
 </div>
 </div>
@@ -105,7 +105,7 @@
 <div class="row">
 <div class="col-lg-6">
 <div class="section-title">
-<h2>Our Concept & artists</h2>
+<h2>Notre concept et nos artistes</h2>
 </div>
 </div>
 <div class="col-lg-6">
@@ -113,28 +113,22 @@
 </div>
 </div>
 <div class="row">
-<div class="col-lg-3 col-sm-6">
+<div class="col-lg-4 col-sm-6">
 <div class="concept-item">
 <img src="https://preview.colorlib.com/theme/solmusic/img/concept/1.jpg" alt="">
-<h5>Soul Music</h5>
+<h5>Musique</h5>
 </div>
 </div>
-<div class="col-lg-3 col-sm-6">
+<div class="col-lg-4 col-sm-6">
 <div class="concept-item">
 <img src="https://preview.colorlib.com/theme/solmusic/img/concept/2.jpg" alt="">
-<h5>Live Concerts</h5>
+<h5>Photo</h5>
 </div>
 </div>
-<div class="col-lg-3 col-sm-6">
+<div class="col-lg-4 col-sm-6">
 <div class="concept-item">
 <img src="https://preview.colorlib.com/theme/solmusic/img/concept/3.jpg" alt="">
-<h5>Dj Sets</h5>
-</div>
-</div>
-<div class="col-lg-3 col-sm-6">
-<div class="concept-item">
-<img src="https://preview.colorlib.com/theme/solmusic/img/concept/4.jpg" alt="">
-<h5>Live Streems</h5>
+<h5>Video</h5>
 </div>
 </div>
 </div>
@@ -147,10 +141,10 @@
 <div class="row">
 <div class="col-lg-6">
 <div class="sub-text">
-<h2>Subscription from $15/month</h2>
-<h3>Start a free trial now</h3>
+<h2>Souscrire pour 15£/mois<h2>
+<h3>Commencer l'essaie gratuit</h3>
 <p>Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-<a href="#" class="site-btn">Try it now</a>
+<a href="#" class="site-btn">Essayer maintenant</a>
 </div>
 </div>
 <div class="col-lg-6">
@@ -173,7 +167,7 @@
 <div class="row">
 <div class="col-lg-6">
 <div class="section-title">
-<h2>Why go Premium</h2>
+<h2>Pourquoi être premium</h2>
 </div>
 </div>
 <div class="col-lg-6">
@@ -181,31 +175,24 @@
 </div>
 </div>
 <div class="row">
-<div class="col-lg-3 col-sm-6">
+<div class="col-lg-4 col-sm-6">
 <div class="premium-item">
 <img src="https://preview.colorlib.com/theme/solmusic/img/premium/1.jpg" alt="">
-<h4>No ad interruptions </h4>
+<h4>Absence de publicité </h4>
 <p>Consectetur adipiscing elit</p>
 </div>
 </div>
-<div class="col-lg-3 col-sm-6">
+<div class="col-lg-4 col-sm-6">
 <div class="premium-item">
 <img src="https://preview.colorlib.com/theme/solmusic/img/premium/2.jpg" alt="">
-<h4>High Quality</h4>
+<h4>Bonne qualité</h4>
 <p>Ectetur adipiscing elit</p>
 </div>
 </div>
-<div class="col-lg-3 col-sm-6">
-<div class="premium-item">
-<img src="https://preview.colorlib.com/theme/solmusic/img/premium/3.jpg" alt="">
-<h4>Listen Offline</h4>
-<p>Sed do eiusmod tempor </p>
-</div>
-</div>
-<div class="col-lg-3 col-sm-6">
+<div class="col-lg-4 col-sm-6">
 <div class="premium-item">
 <img src="https://preview.colorlib.com/theme/solmusic/img/premium/4.jpg" alt="">
-<h4>Download Music</h4>
+<h4>Téléchargement de Music</h4>
 <p>Adipiscing elit</p>
 </div>
 </div>
